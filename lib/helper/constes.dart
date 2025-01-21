dc26ebd4847c2ef892e1275bc12d641851db6ebb
Reza <rezahajrahimi@gmail.com>
2025-01-21 08:02:34 +0330

    Update project version to 4.5.0; modify AndroidManifest.xml to change app icon reference; update iOS and macOS app icons and asset catalog; adjust web manifest background and theme colors; enhance various icon assets across platforms.

--- lib/helper/constes.dart
+++ lib/helper/constes.dart
@@ -1,5 +1,5 @@
 const String projectName = " Power Proxy Seller ";
-const String projectVersion = "4.4.0";
+const String projectVersion = "4.5.0";
 const String projectDescription =
     "Best soloution for selling v2ray vpn by telegram with advanced tools";
 const String appLink =
