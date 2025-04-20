54097c4aa13864eaa57b6c21f5ab42ad843c4189
Reza <rezahajrahimi@gmail.com>
2025-04-20 10:34:43 +0330

    به‌روزرسانی نسخه پروژه به 6.1.0 در فایل‌های constes.dart و pubspec.yaml

--- lib/helper/constes.dart
+++ lib/helper/constes.dart
@@ -1,5 +1,5 @@
 const String projectName = " Power Proxy Seller ";
-const String projectVersion = "6.0.7";
+const String projectVersion = "6.1.0";
 const String projectDescription =
     "Best soloution for selling v2ray vpn by telegram with advanced tools";
 const String appLink =
